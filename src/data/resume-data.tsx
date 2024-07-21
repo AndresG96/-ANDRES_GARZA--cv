4e7e03fb37b7d5f29e2ac9f4217d668069cfc4ac
AndresG96 <amgarza.enterprises@gmail.com>
2024-07-21 15:58:40 -0500

    Initial commit

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -23,10 +23,14 @@ export const RESUME_DATA = {
   name: "Andres Garza",
   initials: "AG",
   location: "Dallas/Fort Worth, Texas, USA",
-  about: "Versatile professional with over seven years of experience in client services, sales, and operations management. Adept at leveraging diverse skill sets to drive business growth and operational efficiency. Seeking opportunities to apply my expertise in sales strategy, team leadership, or multifaceted operational roles.",
+  locationLink: "https://www.google.com/maps/place/Dallas-Fort+Worth",
+  about: "Empowering your business growth while simplifying your journey.",
+  summary: "Versatile professional with over seven years of experience in client services, sales, and operations management. Using diverse skill sets to drive business growth and operational efficiency.",
+  avatarUrl: "https://media.licdn.com/dms/image/D5603AQF-ezUdaRWU9g/profile-displayphoto-shrink_200_200/0/1721590449738?e=1727308800&v=beta&t=2m5Wa2glX6U1qtmkLkeMdfDvVPhMPpy29Tp-yTOYab4",
+  personalWebsiteUrl: "",
   contact: {
     email: "amgarza953@gmail.com",
-    tel: "469-922-4998",
+    tel: "+14699224998",
     social: [
       {
         name: "GitHub",
@@ -45,6 +49,46 @@ export const RESUME_DATA = {
       },
     ],
   },
+  education: [
+    {
+      school: "NOX Campus",
+      degree: "Software Development",
+      start: "2024",
+      end: null,
+    },
+  ],
+  work: [
+    {
+      company: "It's Finished, LLC",
+      link: "",
+      badges: ["Dallas/Fort Worth"],
+      title: "General Manager",
+      start: "2019",
+      end: null,
+      description:
+        "Start up, 3rd party marketer, providing a Mobile Team to execute Ford Airbag Recalls out in residents or commercial campuses\n● Manage relationship with supplier\n● Track and maintain 500 piece inventory\n● Oversee and develop staff of 30 to hit company and individual quota\n● Recruit and train new hires and new promotions\n● Execute personal sales quota\n● Oversee accounting (payroll and company overhead)",
+    },
+    {
+      company: "EVO Payments",
+      link: "",
+      badges: ["Dallas/Fort Worth"],
+      title: "Team Lead and Account Manager",
+      start: "2017",
+      end: "2019",
+      description:
+        "Credit Card processing\n● Recruited and trained new hires for full commission door to door role\n● Oversaw and developed assigned team to hit individual and team quota\n● Execute personal Account acquisition quota",
+    },
+    {
+      company: "Rusty Wallis Honda",
+      link: "",
+      badges: ["Dallas/Fort Worth"],
+      title: "Sales Consultant",
+      start: "2015",
+      end: "2017",
+      description:
+        "Car Sales\n● Helped seekers find a vehicle best fit for their desire and needs (new or used)\n● Negotiated overall price and or payment terms\n● Made daily calls, setting appointments for potential buyers",
+    },
+  ],
   skills: [
     "Microsoft and Google Suites",
     "Supervisory and General Management",
@@ -52,54 +96,16 @@ export const RESUME_DATA = {
     "Negotiating",
     "Recruiting",
   ],
-  work: [
-    {
-      company: "It's Finished, LLC",
-      link: "",
-      badges: [],
-      title: "General Manager",
-      start: "Dec 2019",
-      end: "PRESENT",
-      description: "Start up, 3rd party marketer, providing a Mobile Team to execute Ford Airbag Recalls out in residents or commercial campuses\n● Manage relationship with supplier\n● Track and maintain 500 piece inventory\n● Oversee and develop staff of 30 to hit company and individual quota\n● Recruit and train new hires and new promotions\n● Execute personal sales quota\n● Oversee accounting (payroll and company overhead)",
-    },
-    {
-      company: "EVO Payments",
-      link: "",
-      badges: [],
-      title: "Team Lead and Account Manager",
-      start: "Sept 2017",
-      end: "Dec 2019",
-      description: "Credit Card processing\n● Recruited and trained new hires for full commission door to door role\n● Oversaw and developed assigned team to hit individual and team quota\n● Execute personal Account acquisition quota",
-    },
-    {
-      company: "Rusty Wallis Honda",
-      link: "",
-      badges: [],
-      title: "Sales Consultant",
-      start: "Aug 2015",
-      end: "August 2017",
-      description: "Car Sales\n● Helped seekers find a vehicle best fit for their desire and needs (new or used)\n● Negotiated overall price and or payment terms\n● Made daily calls, setting appointments for potential buyers",
-    },
-  ],
-  education: [
-    {
-      school: "NOX Campus ", // Replace with your actual university name
-      degree: "Software Development", // Replace with your actual degree
-      start: "2024", // Replace with your actual start year
-    },
-    // Add more education entries if needed
-  ],
   projects: [
     {
       title: "E-Commerce Store for Saltgrass Steakhouse",
-      techStack: ["Vercel, Github, Next.js, Tailwind CSS, TypeScript"],
+      techStack: ["Vercel", "Github", "Next.js", "Tailwind CSS", "TypeScript"],
       description: "E-commerce store for a steakhouse that allows users to order merchandise and pay online. It is a prototype and is not yet live.",
-      logo: ConsultlyLogo, // Make sure this logo is imported or replace with an appropriate one
+      logo: ConsultlyLogo,
       link: {
         label: "saltgrass.vercel.app",
         href: "https://saltgrass.vercel.app/",
       },
     },
-    // Add more project entries as needed
   ],
 } as const;
